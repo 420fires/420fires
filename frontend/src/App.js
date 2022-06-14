e31017fe67098bf70d02ec7cc744d4e56e85f169
420fires <fourtwentyfires@gmail.com>
2022-06-14 23:37:16 +0200

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">420fires</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
